b85715640f63433a5d7f23356520c037bc10d916
John Coleman <john-coleman@users.noreply.github.com>
2013-05-14 18:05:48 +0100

    TD-724 Updating pantry to be able to properly use SSH private key from a users data bag item specified from the pantry user attribute. Added bash as the default shell for the user data bag items as I was sick of the basic Bourne sh and no tab-completion

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,15 +14,17 @@ directory "#{node['pantry']['deploy_path']}/bin" do
   recursive true
 end
 
-template "#{node['pantry']['deploy_path']}/bin/wrap-ssh4git.sh" do
-  source "wrap-ssh4git.sh.erb"
-  owner node['pantry']['user']
-  mode 00700
-  variables(
-      :deploy_path => node['pantry']['deploy_path'],
-      :deploy_key => node['pantry']['deploy_key']
-  )
-end
+#template "#{node['pantry']['deploy_path']}/bin/wrap-ssh4git.sh" do
+#  source "wrap-ssh4git.sh.erb"
+#  owner node['pantry']['user']
+#  mode 00700
+#  variables(
+#      :deploy_path => node['pantry']['deploy_path'],
+#      :deploy_key => node['pantry']['deploy_key']
+#  )
+#end
+
+deploy_user = data_bag_item('users', node['pantry']['user'])
 
 application "pantry" do
   repository node['pantry']['repo']
@@ -30,8 +32,8 @@ application "pantry" do
   group node['pantry']['group']
   path node['pantry']['deploy_path']
   environment_name node['pantry']['deploy_environment']
-  #deploy_key "{#node['pantry']['deploy_path']}/.ssh/#{node['pantry']['deploy_key']}"
-  ssh_wrapper "#{node['pantry']['deploy_path']}/bin/wrap-ssh4git.sh"
+  deploy_key deploy_user['ssh_private_key']
+  #ssh_wrapper "#{node['pantry']['deploy_path']}/bin/wrap-ssh4git.sh"
 
   rails do
       database do

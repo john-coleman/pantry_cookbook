152f504e3608af9c07d96e3ddfdf9d45ab0bd551
John Coleman <john-coleman@users.noreply.github.com>
2013-05-16 18:32:44 +0100

    TD-724 Adding mysql database creation. database cookbook to follow

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,27 +21,39 @@ db_database = node['pantry']['database_name']
 db_username = node['pantry']['database_username']
 db_password = node['pantry']['database_password']
 
+# Create the database and user
+mysql_connection_info = {:host => "localhost",
+                         :username => 'root',
+                         :password => node['mysql']['server_root_password']}
+
+mysql_database_user db_username do
+    connection mysql_connection_info
+    password db_password
+    database_name db_database
+    privileges [:all]
+    action :create
+end
+
+mysql_database db_database do
+    connection mysql_connection_info
+    action :create
+end
 
 # Fallback to attribute if chef solo, otherwise use the database_master_role to return an IP from a chef search
 #database_master = node['pantry']['database_host'] 
 
 application "pantry" do
-    repository          node['pantry']['repo']
-    owner               node['pantry']['user']
-    group               node['pantry']['group']
-    path                app_path
-    environment_name    app_env
-    deploy_key          deploy_user_item['ssh_private_key']
-    packages            [ "libxml2-dev",
-                        "libxslt1-dev",
-                        "libmysqlclient-dev",
-                        "libcurl4-openssl-dev",
-                        "libpcre3-dev" ]
-    action              :force_deploy
-    #restart_command "bundle exec rails server -e #{node['pantry']['deploy_environment']}"
+    repository node['pantry']['repo']
+    owner node['pantry']['user']
+    group node['pantry']['group']
+    path app_path
+    environment_name app_env
+    deploy_key deploy_user_item['ssh_private_key']
+    packages [ "libxml2-dev", "libxslt1-dev", "libmysqlclient-dev", "libcurl4-openssl-dev", "libpcre3-dev" ]
+    action :force_deploy
 
     rails do
-        gems [ "bundler" ]
+        gems [ "bundler", "passenger", "unicorn" ]
         database_master_role node['pantry']['database_master_role']
         #restart_command "cd #{app_path}/current; bundle exec rails server -e #{app_env} -d -p #{app_port} -P #{app_pid}"
         database do
@@ -55,6 +67,11 @@ application "pantry" do
     #unicorn do
     #    worker_processes 2
     #end
+    
+    passenger_apache2 do
+        server_aliases [ "pantry.example.com", "pantry" ]
+        webapp_template "pantry_apache.conf.erb"
+    end
 end
 
 
